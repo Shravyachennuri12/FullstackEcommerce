4243a39b0bf9bc435b58a8cacaf72d97d1803789
Shravyachennuri12 <113020823+Shravyachennuri12@users.noreply.github.com>
2025-01-08 13:07:16 +0530

    change cors origin

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-app.use(cors({ origin: 'http://localhost:5173' })); 
+app.use(cors({ origin: '*' })); 
 app.use(bodyParser.json());
 
 
